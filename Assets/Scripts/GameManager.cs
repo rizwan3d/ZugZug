45d72e2253816836d85e5e0bdaa5fb1f8befbca3
Muhammad Rizwan <allgame45@hotmail.com>
2022-08-02 03:06:42 +0500

    Full Game

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,88 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    public float slowDownFactor = 10f;
+    public float slowMotionTime = 1f;
+    public bool gameOver = false;
+    public bool freezeTiles = false;
+    public bool gameEnded = false;
+
+    private int _score = 0;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(this.gameObject);
+    }
+
+    private void Start()
+    {
+        gameOver = true;
+        freezeTiles = true;
+        PlayerPrefs.GetInt("TopScore", 0);
+        gameEnded = false;
+        UIManager.instance.ArriveIntoGame();
+    }
+
+    public void StartGame()
+    {
+        gameOver = false;
+        _score = 0;
+    }
+
+    public void IncrementScore(int increment)
+    {
+        _score += increment;
+        UIManager.instance.UpdateScoreText(this._score);
+    }
+
+    public void EndGame()
+    {
+        gameOver = true;
+        UIManager.instance.ScoreFadeOut();
+        TopScore();
+        StartCoroutine(SlowDownAndStop());
+    }
+
+    private void TopScore()
+    {
+        if (_score > PlayerPrefs.GetInt("TopScore", 0))
+        {
+            PlayerPrefs.SetInt("TopScore", _score);
+        }
+    }
+
+    private IEnumerator SlowDownAndStop()
+    {
+        Time.timeScale = 1 / slowDownFactor;
+        Time.fixedDeltaTime = Time.fixedDeltaTime / slowDownFactor;
+
+        yield return new WaitForSeconds(slowMotionTime / slowDownFactor);
+
+        Time.timeScale = 1;
+        Time.fixedDeltaTime = Time.fixedDeltaTime * slowDownFactor;
+        freezeTiles = true;
+        gameEnded = true;
+
+        UIManager.instance.SetFinalScoreBoard(_score, PlayerPrefs.GetInt("TopScore", 0));
+    }
+
+    public int GetBestScore()
+    {
+        return PlayerPrefs.GetInt("TopScore", 0);
+    }
+
+    public void ReloadLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,118 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    public static TileManager instance;
+
+    public GameObject topTilePrefab;
+    public GameObject leftTilePrefab;
+
+    public GameObject currentTile;
+
+    private Stack<GameObject> _topTiles = new Stack<GameObject>();
+    private Stack<GameObject> _leftTiles = new Stack<GameObject>();
+
+    [SerializeField]
+    Color[] colorList; 
+     public FilterMode filterMode = FilterMode.Point;
+    public TextureWrapMode wrapMode = TextureWrapMode.Clamp;
+    public bool isLinear = false;
+    public bool hasMipMap = false;
+
+
+    int nextIndex = -1;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        CreateTiles(100);
+
+        for (int i = 0; i < 50; i++)
+            instance.SpawnTile();
+    }
+
+    public void CreateTiles(int amount)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            _topTiles.Push(Instantiate(topTilePrefab));
+            _leftTiles.Push(Instantiate(leftTilePrefab));
+            _topTiles.Peek().SetActive(false);
+            _leftTiles.Peek().SetActive(false);
+        }
+    }
+
+    public void SpawnTile()
+    {
+        if (_leftTiles.Count <= 0 || _topTiles.Count <= 0)
+        {
+            CreateTiles(100);
+        }
+
+        int index = Random.Range(0, 2);
+        if (index == 0)
+        {
+            GameObject temp = _topTiles.Pop();
+            temp.SetActive(true);
+            temp.transform.position = currentTile.transform.GetChild(0).GetChild(index).position;
+            currentTile = temp;
+        }
+        else if (index == 1)
+        {
+            GameObject temp = _leftTiles.Pop();
+            temp.SetActive(true);
+            temp.transform.position = currentTile.transform.GetChild(0).GetChild(index).position;
+            currentTile = temp;
+        }
+
+        int pickupIndex = Random.Range(0, 10);
+        if (pickupIndex == 0)
+        {
+            currentTile.transform.GetChild(1).gameObject.SetActive(true);
+        }
+
+        var cubeRenderer = currentTile.transform.GetChild (0).gameObject.GetComponent<Renderer>();
+
+        int colorIndex = nextIndex < 0 ? 0 : nextIndex;     
+
+        Color[] newColorList = new Color[2];
+        newColorList[0] = colorList[colorIndex];
+
+
+        nextIndex = Random.Range(0,colorList.Length);
+
+        while(nextIndex == colorIndex){
+            nextIndex = Random.Range(0,colorList.Length);
+        }
+
+        newColorList[1] = colorList[nextIndex];
+
+
+        currentTile.transform.GetChild (0).gameObject.GetComponent<MeshRenderer>().material.mainTexture = GradientTextureMaker.Create(newColorList, wrapMode, filterMode, isLinear, hasMipMap);
+        //cubeRenderer.material.SetColor("_Color", colorList[colorIndex]);
+    }
+
+    public void AddTopTile(GameObject obj)
+    {
+        _topTiles.Push(obj);
+    }
+
+    public void AddLeftTile(GameObject obj)
+    {
+        _leftTiles.Push(obj);
+    }
+}
